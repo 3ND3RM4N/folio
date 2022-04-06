7671a8874ca77b1fb5d67de202ff40fb4681b430
3ND3RM4N <isangam9@gmail.com>
2022-04-07 00:26:14 +0530

    Navbar and tabs navigation added

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $(".navbut").on("click", function () {
-        console.info(this);
+        //console.info();
         $(this).siblings().removeClass("clicked");
         $(this).addClass("clicked");
+        $("."+$(this).attr("value")).addClass("show").siblings().removeClass("show");
     });
 });
